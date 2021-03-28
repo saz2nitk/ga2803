a231a8253a0bf489e4823daafded195f483727af
saz2nitk <saz2nitk@gmail.com>
2021-03-28 16:20:03 +0530

    created Preprocess module

--- bin/loadData.py
+++ bin/loadData.py
@@ -9,18 +9,24 @@ class ReadArticles:
     
     def __init__(self):
         self.dataPath = r'D:\Academia\Learning\GreyAtom\ga2803\data\train\article1.txt'
+        self.stopWordPath = r'D:\Academia\Learning\GreyAtom\ga2803\data\stopWords.txt'
             
     def loadArticlesFromFile(self):
         with open(self.dataPath,'r') as fl:
             text = fl.read()
         return text
     
+    def stopWordsFromFile(self):
+        with open(self.stopWordPath,'r') as fl:
+            text = fl.read()
+        return text.split('\n')
+        
     def loadArticlesFromDb(self):
         pass
     
     def loadArticlesFromApi(self):
         pass
     
-readObj = ReadArticles()
-articleText = readObj.loadArticlesFromFile()
-print('The text is:\n\n{}'.format(articleText))
+#readObj = ReadArticles()
+#articleText = readObj.loadArticlesFromFile()
+#print('The text is:\n\n{}'.format(articleText))
---
+++ bin/preProcessor.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 28 15:44:27 2021
+
+@author: saz2n
+"""
+import re
+from loadData import ReadArticles
+
+class PreprocessText:
+    
+    def __init__(self):
+        
+        self.specialChars = [',',';','#','$']
+        readObj = ReadArticles()
+        self.articleText = readObj.loadArticlesFromFile()
+        self.stopWords = readObj.stopWordsFromFile()
+        
+    def sentTokenize(self,text):
+        """Tokenize sentences from article.
+        
+        Input:
+            text: string
+        Output:
+            sentArray: a list of strings
+        """
+        sentArray = text.split('.')
+        return sentArray
+    
+    def wordTokenize(self,text):
+        
+        return re.split(' |,|;|-',text)
+    
+    def removeStopWords(self,text):
+        
+        wordList = self.wordTokenize(text)
+        filteredWordList = [word for word in wordList if word not in self.stopWords]
+        sentence = ' '.join(filteredWordList)
+        return sentence
+    
+    def removeSpecialChar(self,text):
+        
+        sentence = ''.join([char for char in text if char not in self.specialChars])
+        return  sentence
+    
+    def convertToLC(self,text):
+        
+        return text.lower()
+    
+    def preprocess(self):
+        text = self.articleText
+        sentences = self.sentTokenize(text)
+        processedSentList = []
+        for sentence in sentences:
+            text = self.convertToLC(sentence)
+            text = self.removeSpecialChar(text)
+            text = self.removeStopWords(text)
+            processedSentList.append(text)
+        return processedSentList
+    
+processObj = PreprocessText()
+processedSentList = processObj.preprocess()
+print('List of processed sentences: \n\n{}'.format(processedSentList))
+    
+    
