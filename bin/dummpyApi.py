d49e3aadc846e0444157c27887746f905280c1c5
Sajjad Hussain <saz2nitk@gmail.com>
2021-04-04 12:18:00 +0530

    added POST dummyapI

--- bin/dummpyApi.py
+++ bin/dummpyApi.py
@@ -7,6 +7,7 @@ Created on Sun Apr  4 11:18:09 2021
 
 import flask
 from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
@@ -18,4 +19,13 @@ def printYay():
 def printWork():
     return "I am going to work"
 
-app.run(host='127.0.0.1',port=8080, debug=True)
+@app.route('/home/custom',methods=['POST'])
+def printCustomTextLower():
+#    text = request.data
+#    text = text.decode()
+#    text = json.loads(text)
+    text = json.loads(request.data.decode())['textData']
+    #print(text)
+    return text.lower()
+
+app.run(host='127.0.0.1',port=8080)
